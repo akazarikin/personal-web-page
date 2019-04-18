0dbe8a54d2e2dc5fdbc81e7a4a4db3880f7aa04a
snear <akazarikin@gmail.com>
2019-04-18 11:38:10 +0300

    Change allow for index page

--- src/main/java/com/snear/personalpage/controllers/WebController.java
+++ src/main/java/com/snear/personalpage/controllers/WebController.java
@@ -2,10 +2,11 @@ package com.snear.personalpage.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class WebController {
-    @RequestMapping(value = "/index")
+    @RequestMapping(value = "/index", method = RequestMethod.GET)
     public String index() {
         return "index";
     }
