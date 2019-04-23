9e6ac97101406510b684769b71a4ab5ed1af152b
snear <akazarikin@gmail.com>
2019-04-23 13:34:14 +0300

    Try to find bug with double call download

--- src/main/java/com/snear/personalpage/controllers/FileDownloadController.java
+++ src/main/java/com/snear/personalpage/controllers/FileDownloadController.java
@@ -1,12 +1,11 @@
 package com.snear.personalpage.controllers;
 
-import org.springframework.core.io.InputStreamResource;
+import org.apache.tomcat.util.http.fileupload.IOUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletResponse;
-import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 
@@ -16,14 +15,23 @@ import java.io.IOException;
 public class FileDownloadController {
 
     @RequestMapping(value = "resume", method = RequestMethod.GET)
-    public File FileSystemResource(HttpServletResponse response) throws IOException {
+    public void FileSystemResource(HttpServletResponse response) throws IOException {
+
         response.setContentType("application/msword");
         response.setHeader("Content-Disposition", "attachment; filename=\"CV_Aleksey_Kazarikin_Java_BE_Developer.docx\"");
         String pathDownloadFile = "/app/target/classes/static/downloads/docs/CV_doc.docx";
 //        String pathDownloadFile = "src/main/resources/static/downloads/docs/CV_doc.docx";
 
-//        InputStreamResource resource = new InputStreamResource(new FileInputStream(pathDownloadFile));
-        File file = new File(pathDownloadFile);
-        return file;
+        try {
+            FileInputStream fileInputStream = new FileInputStream(pathDownloadFile);
+
+            IOUtils.copy(fileInputStream, response.getOutputStream());
+            response.flushBuffer();
+
+        } catch (IOException ex) {
+            System.out.println("Error writing file to output stream. Filename ");
+            throw new RuntimeException("IOError writing file to output stream");
+        }
+
     }
 }
