dc5cf57d5cb1a5474418297af17aba6d49e95a58
snear <akazarikin@gmail.com>
2019-04-23 10:49:49 +0300

    Revert "Fix issue with download from mobile version website"
    This reverts commit 264e6022

--- src/main/java/com/snear/personalpage/controllers/WebController.java
+++ src/main/java/com/snear/personalpage/controllers/WebController.java
@@ -1,36 +1,29 @@
 package com.snear.personalpage.controllers;
 
-import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+import springfox.documentation.schema.Model;
 
-import java.net.HttpCookie;
-import java.util.List;
-import java.util.Map;
+import java.security.Principal;
 
 @Controller
 public class WebController {
     @RequestMapping(value = "/", method = RequestMethod.GET)
-    public String index(@RequestHeader HttpHeaders headers, @RequestParam Map<String, String> allParams) {
-        System.out.println("=================================================================================");
-        System.out.println(headers.getLocation());
-        System.out.println("=================================================================================");
-        headers.entrySet().forEach(stringListEntry -> System.out.println(stringListEntry));
-        System.out.println("=================================================================================");
-        System.out.println("=================================================================================");
-        allParams.forEach((s, s2) -> System.out.println("key : " + s + ";  " + "Value : " + s2));
-        System.out.println("=================================================================================");
-        System.out.println("=================================================================================");
+    public String index(Principal principal, @CookieValue(value = "myCookieName", defaultValue = "defaultCookieValue") String cookieValue, Model model) {
 
-//        List<HttpCookie> cookies = HttpCookie.parse(headers.);
-//        cookies.forEach(httpCookie -> System.out.println(httpCookie));
-        System.out.println("=================================================================================");
-        System.out.println("=================================================================================");
+        System.out.println(cookieValue);
 
+        if (principal != null) {
+            System.out.println(principal.toString());
+        } else if (model != null) {
+            System.out.println(model.toString());
+        } else {
+            System.out.println("principal = null");
+        }
 
-        return "base";
+//        return principal != null ? "home/homeSignedIn" : "home/homeNotSignedIn";
+        return "index";
     }
 }
