f701a25dd5d64b777fd7362f88e79978d2c8d7fb
snear <akazarikin@gmail.com>
2019-04-23 11:14:24 +0300

    fixup! Fix issue with start page

--- src/main/java/com/snear/personalpage/controllers/WebController.java
+++ src/main/java/com/snear/personalpage/controllers/WebController.java
@@ -4,26 +4,15 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import springfox.documentation.schema.Model;
 
-import java.security.Principal;
 
 @Controller
 public class WebController {
     @RequestMapping(value = "/", method = RequestMethod.GET)
-    public String index(Principal principal, @CookieValue(value = "myCookieName", defaultValue = "defaultCookieValue") String cookieValue, Model model) {
+    public String index(@CookieValue(value = "myCookieName", defaultValue = "defaultCookieValue") String cookieValue) {
 
         System.out.println(cookieValue);
 
-        if (principal != null) {
-            System.out.println(principal.toString());
-        } else if (model != null) {
-            System.out.println(model.toString());
-        } else {
-            System.out.println("principal = null");
-        }
-
-//        return principal != null ? "home/homeSignedIn" : "home/homeNotSignedIn";
-        return "index";
+        return "base";
     }
 }
