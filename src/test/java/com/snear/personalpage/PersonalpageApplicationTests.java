9ea010df9b1253cb1e00e718eb975ac5ff5c9d9e
Alex <akazarikin@gmail.com>
2025-01-31 19:21:19 +0200

    Update PersonalpageApplicationTests.java
    added JUnit5

--- src/test/java/com/snear/personalpage/PersonalpageApplicationTests.java
+++ src/test/java/com/snear/personalpage/PersonalpageApplicationTests.java
@@ -1,16 +1,13 @@
 package com.snear.personalpage;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test; // Импортируйте JUnit 5
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest
-public class PersonalpageApplicationTests {
+class PersonalpageApplicationTests {
 
     @Test
-    public void contextLoads() {
+    void contextLoads() {
+        // Тестовый метод
     }
-
 }
