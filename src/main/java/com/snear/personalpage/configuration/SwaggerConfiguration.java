bd2dec82ee7c27a24ee7a58bd039caf8a7c62ad6
Alex <akazarikin@gmail.com>
2025-01-31 22:21:45 +0200

    Update SwaggerConfiguration.java
    update old Springfox to new Springdoc OpenAPI

--- src/main/java/com/snear/personalpage/configuration/SwaggerConfiguration.java
+++ src/main/java/com/snear/personalpage/configuration/SwaggerConfiguration.java
@@ -1,71 +1,19 @@
 package com.snear.personalpage.configuration;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import springfox.documentation.builders.ParameterBuilder;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.schema.ModelRef;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Parameter;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger.web.ApiKeyVehicle;
-import springfox.documentation.swagger.web.SecurityConfiguration;
-import springfox.documentation.swagger.web.UiConfiguration;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
-import java.util.Arrays;
-import java.util.List;
 
 @Configuration
-@EnableSwagger2
-public class SwaggerConfiguration {
+public class OpenApiConfig {
 
     @Bean
-    public Docket api() {
-
-        return new Docket(DocumentationType.SWAGGER_2)
-                .select()
-                .apis(RequestHandlerSelectors.any())
-                .paths(PathSelectors.any())
-                .build()
-                .pathMapping("/")
-                .globalOperationParameters(getHeader())
-                .apiInfo(apiInfo());
-    }
-
-    private List<Parameter> getHeader() {
-        return Arrays.asList(new ParameterBuilder()
-                .name("Authorization")
-                .required(false)
-                .modelRef(new ModelRef("string"))
-                .parameterType("header")
-                .defaultValue("")
-                .build());
-    }
-
-
-    private ApiInfo apiInfo() {
-        return ApiInfo.DEFAULT;
-    }
-
-    @Bean
-    public SecurityConfiguration security() {
-        //Setting swagger to use authorization header when typing appkey in the top of the swagger screen
-        return new SecurityConfiguration(
-                null,
-                null,
-                null,
-                null,
-                "",
-                ApiKeyVehicle.HEADER,
-                "Authorization",
-                ",");
-    }
-
-    @Bean
-    public UiConfiguration uiConfig() {
-        return UiConfiguration.DEFAULT;
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Personal Page API")
+                        .version("1.0")
+                        .description("API documentation for Personal Page"));
     }
 }
